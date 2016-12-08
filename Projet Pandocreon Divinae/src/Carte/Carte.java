8a9dd0191db3725b516cbeb92cd772e7e7f37460
Theo GUIDET <theo.guidet@hotmail.fr>
2016-12-08 03:19:26 +0100

    bug dans partie pour recuperation nbpriere des cartes croyants au moment du calcul

--- Projet Pandocreon Divinae/src/Carte/Carte.java
+++ Projet Pandocreon Divinae/src/Carte/Carte.java
@@ -13,5 +13,39 @@ public class Carte {
 	public void utiliserCapacite(){
 		
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public TypeCarte getTypeCarte() {
+		return typeCarte;
+	}
+
+	public void setTypeCarte(TypeCarte typeCarte) {
+		this.typeCarte = typeCarte;
+	}
+
+	public String getCapacite() {
+		return capacite;
+	}
+
+	public void setCapacite(String capacite) {
+		this.capacite = capacite;
+	}
+
+	public boolean isEstDistribuee() {
+		return estDistribuee;
+	}
+
+	public void setEstDistribuee(boolean estDistribuee) {
+		this.estDistribuee = estDistribuee;
+	}
+	
+	
 	
 }
